fbab601aeacf8b46c47a37a30440cf615430ab08
Karlafgl <karlafgl@gmail.com>
2019-08-01 19:09:31 -0600

    first commit

---
+++ js/API.js
@@ -0,0 +1,16 @@
+class API {
+     async obtenerDatos() {
+
+          const total = 1000;
+          // Obtener desde la API
+          const datos = await fetch(`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
+
+          // Retornar como JSON
+          const respuestaJSON = await datos.json();
+
+          // Retornar el objeto
+          return {
+               respuestaJSON
+          }
+     }
+}
